d39f69cb692ae97595e4549d8286b422b3f69414
jaron <kjh_0112@163.com>
2016-04-07 14:07:03 +0800

    Create a directory on the Helloworld repository.

---
+++ python/format.py
@@ -0,0 +1,14 @@
+#-*-coding:utf-8-*-
+name = ('name', 'age')
+print 'print name = %s, age = %s' %name
+
+
+#tuple 函数将列表转化为元组
+arr_name = ['name', 'age']
+print 'print name = %s, age = %s' %tuple(arr_name)
+
+
+dir_name = {'name':'python', 'geeting':'Hello'}
+print '%(geeting)s , %(name)s' %dir_name
+
+print '中文输出！'
